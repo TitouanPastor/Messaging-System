8f9bd98aeb7bde783177e8ed8d84d1b4e3cb19e9
Baptiste Bayche <baptiste.bayche@etu.iut-tlse3.fr>
2023-02-21 10:32:01 +0100

    add files enregistrer.php and recuperer.php

---
+++ php/recuperer.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once('connectToDB.php');
+
+
+$sql = new connectToBD();
+$linkpdo = $sql->getConnection();
+
+$req = $linkpdo->prepare('select * from chat order by timestamp desc limit 10');
+$req->execute();
+
+lalalaa
